64fe183ae571cf61b550f4c926793467f5466f7a
Patrick Shim <patrick.shim91@yahoo.com>
2024-11-05 18:05:06 -0500

    test: added initialize test for stat_Tracker

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,9 +1,12 @@
 # require './spec_helper'
-require './spec/stat_tracker_spec'
+require './lib/stat_tracker'
 
 RSpec.describe StatTracker do 
+  before :each do
+    @stat_tracker = StatTracker.new('./data/games.csv', './data/teams.csv', './data/game_teams.csv')
+  end
+
   it "should exist" do
-    @factory = StatTracker.new()
-    expect(@factory).to be_a StatTracker
+    expect(@stat_tracker).to be_a StatTracker
   end
 end
