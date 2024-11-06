2bd9530a1fbc937b87567b54b3b40bc5549c46ba
Jono Sommers <jonosommers95@gmail.com>
2024-11-06 14:46:32 -0600

    team data names are updated to match CSV

--- lib/team.rb
+++ lib/team.rb
@@ -3,14 +3,14 @@ require 'pry'
 require_relative './stat_tracker'
 
 class Team
-  attr_reader :team_id, :franchiseid, :teamname, :abbreviation, :stadium, :link
+  attr_reader :team_id, :franchiseId, :teamName, :abbreviation, :Stadium, :link
 
   def initialize(data)
     @team_id = data["team_id"]
     @franchiseid = data["franchiseId"]
-    @teamname = data["teamname"]
+    @teamname = data["teamName"]
     @abbreviation = data["abbreviation"]
-    @stadium = data["stadium"]
+    @stadium = data["Stadium"]
     @link = data["link"]
   end
 end
