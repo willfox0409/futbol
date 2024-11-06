4241c2bc2f07307aa5daf68739a345c25332ce2b
Jono Sommers <jonosommers95@gmail.com>
2024-11-06 14:45:43 -0600

    fixing team data names

--- lib/team.rb
+++ lib/team.rb
@@ -3,14 +3,14 @@ require 'pry'
 require_relative './stat_tracker'
 
 class Team
-  attr_reader :team_id, :franchiseId, :teamName, :abbreviation, :Stadium, :link
+  attr_reader :team_id, :franchiseid, :teamname, :abbreviation, :stadium, :link
 
   def initialize(data)
     @team_id = data["team_id"]
     @franchiseid = data["franchiseId"]
-    @teamname = data["teamName"]
+    @teamname = data["teamname"]
     @abbreviation = data["abbreviation"]
-    @stadium = data["Stadium"]
+    @stadium = data["stadium"]
     @link = data["link"]
-  end 
+  end
 end
