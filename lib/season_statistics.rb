31b05096453c4c90dc360700317d8d76a7c5ffaa
Jono Sommers <jonosommers95@gmail.com>
2024-11-11 14:24:34 -0600

    Working on making the winningeset and worst coach methods pass

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -1,9 +1,10 @@
 class SeasonStatistics
-    attr_reader :teams, :game_teams
+    attr_reader :teams, :game_teams, :games
   
-    def initialize(teams, game_teams)
+    def initialize(teams, game_teams, games)
       @teams = teams
       @game_teams = game_teams
+      @games = games
     end
 
     def count_of_teams
